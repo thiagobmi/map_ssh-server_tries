a5fa4fd5ea763545850da37467afbe52806f48d9
thiagobmi <thiagomorais.aluno@unipampa.edu.br>
2023-05-11 03:14:07 -0300

    now it accepts bigger numbers

--- choroplat.py
+++ choroplat.py
@@ -1,7 +1,10 @@
+import numpy as np
 import plotly_express as px
 import pandas as pd
 
-data = []
+data=[]
+count =[]
+country = []
 
 file = open("output.txt", "r")
 bigstring = file.read()
@@ -9,14 +12,28 @@ bigstring = file.read()
 bigstring = bigstring.replace(",", "")
 listas = bigstring.split("\n")
 
-
 for i in listas:
-    temp = i.strip().split(" ")
-    if len(temp) > 1:
-        if temp[1]=='United':
-            temp[1]='USA'
-        if temp[1] != "can't":
-            data.append([temp[1],int(temp[0])])
+    temp = i.split(";")
+    if temp != ['']:
+        count.append([int(temp[0])])
+        country.append([temp[2]])
+
+uniqueCT = np.unique(country)
+
+counter = np.zeros(len(uniqueCT))
+countCT = []
+
+for i in range(0,len(uniqueCT)):
+    for j in range(0,len(country)):
+        if country[j][0] == uniqueCT[i]:
+            counter[i] += int(count[j][0])
+
+for i in range(0,len(counter)):
+    if uniqueCT[i]=='United':
+        uniqueCT[i]='USA'
+    if uniqueCT[i] != "can't" and uniqueCT[i] != "Address" and uniqueCT[i] != "Resolve":
+        data.append([uniqueCT[i],int(counter[i])])
+
 
 df= pd.DataFrame(data, columns=['Country','Tries'])
 
--- newMap.sh
+++ newMap.sh
@@ -1,6 +1,6 @@
-cat /var/log/auth.log* | grep 'Failed password for root' | awk -F ' ' '{print $11}' | sed '/Failed/d' > ./root_tries.txt
+zgrep 'Failed password for root' /var/log/auth.log*.gz  | awk -F ' ' '{print $11}' | sed '/Failed/d' > ./root_tries.txt
 
-cat /var/log/auth.log* | grep Failed | grep 'invalid user' | awk -F ' ' '{print $11,$13}' | awk '{print $2}' > only-ip.txt
+zgrep 'invalid user' /var/log/auth.log* | grep Failed | awk -F ' ' '{print $11,$13}' | awk '{print $2}' > ./only-ip.txt
 
 while IFS= read -r line; #
         do newArr+=($line) #
@@ -10,26 +10,27 @@ while IFS= read -r line;
         do newArr+=($line)
 done < root_tries.txt
 
-most_req=`for i in "${newArr[@]}";do
-	echo $i
-done | sort | uniq -c | sort -nr | head -n1`
 
-ip_most_req=`echo $most_req | awk '{print $2}'`
-reqs_ip=`echo $most_req | awk '{print $1}'`
+for i in "${newArr[@]}";do
+        echo $i
+done | sort | uniq -c | sort -nr > NUM_AND_IP.txt
 
-echo "IP with most tries: $ip_most_req with $reqs_ip requests."
-echo "From `geoiplookup $ip_most_req | cut -d ' ' -f 4-5`"
+while IFS= read -r line; do
+	countArr+=($(echo $line | awk '{print $1}'))
+	IPArr+=($(echo $line | awk '{print $2}'))
+	countryArr+=($(geoiplookup `echo $line | awk '{print $2}'` | cut -d ' ' -f 5))
+done < NUM_AND_IP.txt
 
+echo "IP with most tries: ${IPArr[0]} with ${countArr[0]} requests."
+echo "From `geoiplookup ${IPArr[0]} | cut -d ' ' -f 4-5`"
 
-
-for i in "${newArr[@]}"; do
-        echo "`geoiplookup $i | cut -d ' ' -f 5`"
-done | sort | uniq -c | sort -nr > output.txt
-
+for ((i=0;i<${#IPArr[@]};i++)); do
+	echo "${countArr[i]};${IPArr[i]};${countryArr[i]}"
+done > output.txt
 
 rm only-ip.txt
 rm root_tries.txt
-
+rm NUM_AND_IP.txt
 python3 choroplat.py
 
 rm output.txt
